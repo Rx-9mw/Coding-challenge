7b3d26c26a84bf42c68a8944e2e87c8890c6ec2f
Szymon Pawlowicz <smonek33@gmail.com>
2023-11-23 20:11:36 +0100

    Added a connection to the database

--- app.js
+++ app.js
@@ -2,7 +2,19 @@ const express = require('express');
 
 const app = express();
 
+const mongoose = require('mongoose');
+
 const userRoutes = require('./Routes/User_Routes');
+
+mongoose
+  .connect('mongodb+srv://daznchallengecluster.gkyhlnn.mongodb.net/', {
+    dbName: 'daznchallengecluster',
+    user: 'smonek33',
+    pass: 'ThL34uwsvWevVK3M'
+  })
+  .then(() => {
+    console.log('Database is connected...');
+  });
 
 app.use('/users', userRoutes);
 
@@ -10,4 +22,6 @@ app.all('*', (req, res) => {
   res.status(404).send('Not Found!');
 })
 
-app.listen(3000, '0.0.0.0');
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server is running on port 3000...')
+});
