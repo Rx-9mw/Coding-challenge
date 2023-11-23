3e93b15e705e6ef17bc741c11de3108f1b01dd86
Szymon Pawlowicz <smonek33@gmail.com>
2023-11-23 20:16:41 +0100

    Added more routes for /users

--- Routes/User_Routes.js
+++ Routes/User_Routes.js
@@ -3,7 +3,23 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Getting a list of all users');
+  res.send('Getting a list of all users...');
+});
+
+router.get('/:id', (req, res) => {
+  res.send('Getting the user by id...')
+});
+
+router.post('/', (req, res) => {
+  res.send('Adding new user into the database...')
+});
+
+router.delete('/:id', (req, res) => {
+  res.send('Deleting the user...')
+});
+
+router.patch('/:id', (req, res) => {
+  res.send('Updating user by id...')
 });
 
 module.exports = router;
