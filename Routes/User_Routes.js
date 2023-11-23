77e7b4cc7d477851d55ad0687062046ab3b536b9
Szymon Pawlowicz <smonek33@gmail.com>
2023-11-23 20:53:20 +0100

    Added the get request for a single user and the delete request

--- Routes/User_Routes.js
+++ Routes/User_Routes.js
@@ -8,13 +8,24 @@ router.get('/', async (req, res) => {
   try {
     const allUsers = await User.find({}, {__v: 0});
     res.send(allUsers);
+
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
   }
+
 });
 
-router.get('/:id', (req, res) => {
-  res.send('Getting the user by id...')
+router.get('/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await User.findById(id, {__v: 0});
+    res.send(user);
+
+  } catch (error) {
+    res.send("Couldn't find a user with this id.")
+    console.log(error.message)
+  }
+
 });
 
 router.post('/', async (req, res) => {
@@ -28,17 +39,26 @@ router.post('/', async (req, res) => {
     }
 
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
   }
 
 });
 
-router.delete('/:id', (req, res) => {
-  res.send('Deleting the user...')
+router.delete('/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    await User.findByIdAndDelete(id);
+    res.send('User has been deleted.');
+
+  } catch (error) {
+    res.send("Couldn't find a user with this id.")
+    console.log(error.message);
+  }
+
 });
 
 router.patch('/:id', (req, res) => {
-  res.send('Updating user by id...')
+  
 });
 
 module.exports = router;
